c44e89fc9389f3918c7c9c18019071db08607311
Andris Jansons <jansonsa@coventry.ac.uk>
2020-11-10 16:10:37 +0000

    Added JSON Schema for users and roles.
    - Added jsonschema package
    - JSON Schema middleware validation
    - Using experimental-json-modules to import *.json files
    - Unit tests for schema validation

---
+++ middlewares/validation.js
@@ -0,0 +1,41 @@
+import JsonSchema from 'jsonschema';
+import userSchema from '../schemas/user.json';
+import roleSchema from '../schemas/role.json';
+
+const {user, userUpdate} = userSchema.definitions;
+
+const validator = new JsonSchema.Validator();
+validator.addSchema(userSchema);
+validator.addSchema(roleSchema);
+
+/**
+ * Wrapper that returns Koa middleware validator for a given schema
+ * @param {object} schema JSON Schema definition
+ * @param {string} resource Resource name
+ * @return {function} A koa middleware handler
+ */
+function makeValidator(schema, resource) {
+  return async (ctx, next) => {
+    const body = ctx.request.body;
+    const validationOptions = {
+      throwError: true,
+      propertyName: resource,
+    };
+    try {
+      validator.validate(body, schema, validationOptions);
+      await next();
+    } catch (err) {
+      if (err instanceof JsonSchema.ValidationError) {
+        ctx.status = 400;
+        ctx.body = err;
+      } else {
+        throw err;
+      }
+    }
+  };
+}
+
+/** Validate data against user schema for creating user */
+export const validateUser = makeValidator(user, 'user');
+/** Validate data against user schema for updating user */
+export const validateUserUpdate = makeValidator(userUpdate, 'userUpdate');
